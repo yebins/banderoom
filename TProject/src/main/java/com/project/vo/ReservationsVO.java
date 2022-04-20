28ab81f47ae055cf708b66329e021e7f4128c4e4
Soohyun <Soohyun@LAPTOP-VVLJ6698>
2022-04-20 12:32:21 +0900

    공간VO.java

---
+++ TProject/src/main/java/com/project/vo/ReservationsVO.java
@@ -0,0 +1,15 @@
+package com.project.vo;
+
+import java.util.Date;
+
+public class ReservationsVO {
+
+	private int resIdx;
+	private int mIdx;
+	private int spaceIdx;
+	private int peopleNum;
+	private Date startDate;
+	private Date endDate;
+	private Date resDate;
+	private int cost;
+}
---
+++ TProject/src/main/java/com/project/vo/SpacePicturesVO.java
@@ -0,0 +1,8 @@
+package com.project.vo;
+
+public class SpacePicturesVO {
+
+	private int pictureIdx;
+	private int spaceIdx;
+	private String src;
+}
