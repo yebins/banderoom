7d17e540767cd735604d2af8bada77e08ba908e5
82105 <82105@DESKTOP-N6AC1C3>
2022-04-21 17:47:25 +0900

    dh

---
+++ TProject/src/main/java/com/project/controller/TeamsController.java
@@ -0,0 +1,14 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value="/teams")
+@Controller
+public class TeamsController {
+
+	@RequestMapping(value="/main.do")
+	public String teamsMain() {
+		return "teams/main";
+	}
+}
