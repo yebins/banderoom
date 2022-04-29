583e403a595b4ce344cd511ed28273e07d86ebf0
82105 <82105@DESKTOP-N6AC1C3>
2022-04-29 09:22:09 +0900

    board

--- TProject/src/main/java/com/project/controller/BoardController.java
+++ TProject/src/main/java/com/project/controller/BoardController.java
@@ -1,6 +1,11 @@
 package com.project.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,20 +25,14 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 	
-	@RequestMapping(value="/notice.do")
-	public String Notice(Model model) {
-		
-		return "board/notice";
-	}
-	
 	@RequestMapping(value="/register.do", method=RequestMethod.GET)
-	public String register() {
+	public String register(ArticlesVO vo) {
 		
 		return "board/register";
 	}
 	
 	@RequestMapping(value="/register.do", method=RequestMethod.POST)
-	public String register(Model model,ArticlesVO vo) {
+	public String register(Model model,ArticlesVO vo, HttpServletRequest request) {
 		
 		boardService.insertArticlesVO(vo);
 		
@@ -41,15 +40,41 @@ public class BoardController {
 	}
 	
 	@RequestMapping(value="/list.do",method=RequestMethod.GET)
-	public String list(Model model,Integer bidx,String searchtitle) {
-		System.out.println(bidx);//bidx값 확인
+	public String list(Model model,Integer bidx,String searchtitle, ArticlesVO vo) {
 		System.out.println(searchtitle);//검색어 확인
 		List<ArticlesVO> list=boardService.list(bidx,searchtitle);
 		
 		System.out.println(list.size());
 		model.addAttribute("list",list);
 		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("mm-DD-yyyy");
+		SimpleDateFormat df = new SimpleDateFormat("mm-DD");
+		
+		
+		if(sdf.format(vo.getRegDate())==sdf.format(date)) {
+			String mm = df.format(vo.getRegDate());
+			try {
+				vo.setRegDate(df.parse(mm));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 		return "board/list";
+	}
+	
+	@RequestMapping(value="/details.do")
+	public String details() {
+		
+		return "board/details";
+	}
+	
+	@RequestMapping(value="/update.do", method=RequestMethod.GET)
+	public String update(ArticlesVO vo) {
+		
+		return "board/update";
 	}
 	
 }
