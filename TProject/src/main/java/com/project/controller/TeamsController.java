71a577b89b0ccc324edde33f3263ab64268b45d9
Maazzii <tlwlro@outlook.com>
2022-06-07 11:13:10 +0900

    팀 모집 마감처리 스케줄러로 구현

--- TProject/src/main/java/com/project/controller/TeamsController.java
+++ TProject/src/main/java/com/project/controller/TeamsController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -457,8 +458,7 @@ public class TeamsController {
 		}
 	}
 	
-	@RequestMapping(value="/updateStatus.do")
-	@ResponseBody
+	@Scheduled(cron = " 0 0 0 * * *")
 	public void updateStatus() {
 		teamsService.updateStatus();
 	}
--- TProject/src/main/webapp/WEB-INF/views/teams/main.jsp
+++ TProject/src/main/webapp/WEB-INF/views/teams/main.jsp
@@ -324,18 +324,9 @@ select[name=sort]{
 		}
 	}
 	
-	function updateStatus(){
-		$.ajax({
-			url:"updateStatus.do",
-			type:"post",
-			error:function(){
-				alert('status 변경 실패');
-			}
-		})
-	}
 </script>
 </head>
-<body<c:if test="${login.mIdx==4}"> onload='updateStatus()'</c:if>>
+<body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 	<c:import url="/header.do" />
